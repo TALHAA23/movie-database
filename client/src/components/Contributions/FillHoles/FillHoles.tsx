909879af451c06061d17ef00eec4d06ce0efe168
Talha <talhasifat23@gamil.com>
2024-02-14 08:14:32 +0500

    branch changed finalized

--- client/src/components/Contributions/FillHoles/FillHoles.tsx
+++ client/src/components/Contributions/FillHoles/FillHoles.tsx
@@ -1,12 +1,5 @@
-import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import { useSearchParams } from "react-router-dom";
-import movieByIdApi from "../../../api/movieByIdApi";
-import PageLoader from "../../Loaders/PageLoader";
-import { MovieInterface } from "../../../api/model/Interfaces";
-import CreateInputForMissingFeilds from "../ContributionUtils/CreateInputForMissingFeilds.tsx";
 import "../../../../public/form.css";
-import fillHolesFormSubmission from "../../../api/services/fillHolesFormSubmission.ts";
-import { useEffect } from "react";
 import FillHolesofMovie from "./FillHolesofMovie.tsx";
 import FillHolesofActor from "./FillHolesofActor.tsx";
 
